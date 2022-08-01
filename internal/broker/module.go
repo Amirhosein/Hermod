163f7ae952fa9015df1319b82142adec4463ad2b
Amirhossein Iravanimanesh <airavanimanesh@gmail.com>
2022-08-01 17:40:29 +0430

    feat: Implement required fields and methods
    All test passed

--- internal/broker/module.go
+++ internal/broker/module.go
@@ -2,29 +2,97 @@ package broker
 
 import (
 	"context"
+	"sync"
 	"therealbroker/pkg/broker"
+	"time"
 )
 
 type Module struct {
-	// TODO: Add required fields
+	subscribers   map[string][]chan broker.Message
+	messages      []broker.Message
+	IsClosed      bool
+	ListenersLock sync.Mutex
 }
 
 func NewModule() broker.Broker {
-	return &Module{}
+	return &Module{
+		subscribers:   make(map[string][]chan broker.Message),
+		IsClosed:      false,
+		ListenersLock: sync.Mutex{},
+	}
 }
 
 func (m *Module) Close() error {
-	panic("implement me")
+	if m.IsClosed {
+		return broker.ErrUnavailable
+	}
+
+	m.IsClosed = true
+	return nil
 }
 
 func (m *Module) Publish(ctx context.Context, subject string, msg broker.Message) (int, error) {
-	panic("implement me")
+	if m.IsClosed {
+		return -1, broker.ErrUnavailable
+	}
+
+	for _, listener := range m.subscribers[subject] {
+		if cap(listener) != len(listener) {
+
+			listener <- msg
+		}
+	}
+
+	msg.Id = len(m.messages)
+	// msg.IsExpired = false
+	m.messages = append(m.messages, msg)
+
+	if msg.Expiration != 0 {
+		go func(msg *broker.Message) {
+			time.Sleep(msg.Expiration)
+			m.ListenersLock.Lock()
+			defer m.ListenersLock.Unlock()
+			for i, msg := range m.messages {
+				if msg.Id == i {
+					m.messages = append(m.messages[:i], m.messages[i+1:]...)
+					break
+				}
+			}
+			// msg.IsExpired = true
+		}(&msg)
+	}
+
+	return msg.Id, nil
 }
 
 func (m *Module) Subscribe(ctx context.Context, subject string) (<-chan broker.Message, error) {
-	panic("implement me")
+	if m.IsClosed {
+		return nil, broker.ErrUnavailable
+	}
+
+	select {
+	case <-ctx.Done():
+		return nil, broker.ErrExpiredID
+	default:
+		newChannel := make(chan broker.Message, 100)
+		m.ListenersLock.Lock()
+		m.subscribers[subject] = append(m.subscribers[subject], newChannel)
+		m.ListenersLock.Unlock()
+
+		return newChannel, nil
+	}
 }
 
 func (m *Module) Fetch(ctx context.Context, subject string, id int) (broker.Message, error) {
-	panic("implement me")
+	if m.IsClosed {
+		return broker.Message{}, broker.ErrUnavailable
+	}
+
+	for _, msg := range m.messages {
+		if msg.Id == id {
+			return msg, nil
+		}
+	}
+
+	return broker.Message{}, broker.ErrExpiredID
 }
--- pkg/broker/broker.go
+++ pkg/broker/broker.go
@@ -10,13 +10,15 @@ type Message struct {
 	// This parameter is optional. If it's not provided,
 	// the Message can't be accessible through Fetch()
 	// id is unique per every subject
-	id int
+	Id int
 	// Body of the message
 	Body string
 	// The time that message can be accessible through Fetch()
 	// with the proper Message id
 	// 0 when there is no need to keep message ( fire & forget mode )
 	Expiration time.Duration
+
+	// IsExpired bool
 }
 
 // The whole implementation should be thread-safe
