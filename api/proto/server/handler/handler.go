ec48c8948866ba88f88d09303e82f451b17091c6
Amirhossein Iravanimanesh <airavanimanesh@gmail.com>
2022-08-03 12:08:25 +0430

    feat: Implement gRPC server
    Not completed yet ...

---
+++ api/proto/server/handler/handler.go
@@ -0,0 +1,64 @@
+package handler
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"sync"
+	"therealbroker/api/proto"
+	"therealbroker/pkg/broker"
+	"time"
+)
+
+type Server struct {
+	proto.UnimplementedBrokerServer
+	BrokerInstance broker.Broker
+
+	LastPublishLock *sync.Mutex
+	LastTopicLock   *sync.Mutex
+	LastPublishId   int
+}
+
+func (s *Server) Publish(globalContext context.Context, request *proto.PublishRequest) (*proto.PublishResponse, error) {
+	fmt.Println("inside publish")
+
+	msg := broker.Message{
+		Body:       (string)(request.Body),
+		Expiration: time.Duration(request.ExpirationSeconds) * time.Second,
+	}
+
+	publishId, err := s.BrokerInstance.Publish(globalContext, request.Subject, msg)
+
+	if err != nil {
+		log.Println(err)
+		return nil, err
+	}
+
+	return &proto.PublishResponse{Id: int32(publishId)}, nil
+}
+
+func (s *Server) Subscribe(globalContext context.Context, request *proto.SubscribeRequest) error {
+	fmt.Println("inside subscribe")
+
+	_, err := s.BrokerInstance.Subscribe(globalContext, request.Subject)
+	if err != nil {
+		log.Println(err)
+		return err
+	}
+
+	return nil
+}
+
+func (s *Server) Fetch(ctx context.Context, request *proto.FetchRequest) (*proto.MessageResponse, error) {
+	log.Println("Getting fetch request")
+	defer log.Println("Finish handling fetch request")
+
+	msg, err := s.BrokerInstance.Fetch(ctx, request.Subject, int(request.Id))
+
+	if err != nil {
+		log.Println(err)
+		return nil, err
+	}
+
+	return &proto.MessageResponse{Body: []byte(msg.Body)}, nil
+}
