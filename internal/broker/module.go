dad18dc0e91108e8b325862ba7c55d7f0c6b03fe
Amirhossein Iravanimanesh <airavanimanesh@gmail.com>
2022-08-15 00:50:23 +0430

    feat: Add Cassandra and PostgreSQL option
    commented

--- internal/broker/module.go
+++ internal/broker/module.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"sync"
 	"therealbroker/internal/model"
+	"therealbroker/internal/repository"
 	"therealbroker/pkg/broker"
 	"time"
 )
@@ -11,14 +12,22 @@ import (
 type Module struct {
 	subscribers   map[string]model.Subject
 	messages      map[int]broker.Message
+	db            repository.Database
 	IsClosed      bool
 	ListenersLock sync.RWMutex
 }
 
 func NewModule() broker.Broker {
+	// db, err := repository.GetPostgre()
+	db, err := repository.GetCassandra()
+	if err != nil {
+		panic(err)
+	}
+
 	return &Module{
 		subscribers:   make(map[string]model.Subject),
 		messages:      make(map[int]broker.Message),
+		db:            db,
 		IsClosed:      false,
 		ListenersLock: sync.RWMutex{},
 	}
@@ -48,11 +57,12 @@ func (m *Module) Publish(ctx context.Context, subject string, msg broker.Message
 			}
 		}
 
-		msg.Id = len(m.messages)
+		msg.Id = m.db.SaveMessage(msg, subject)
+		// msg.Id = len(m.messages)
 
-		m.ListenersLock.Lock()
-		m.messages[msg.Id] = msg
-		m.ListenersLock.Unlock()
+		// m.ListenersLock.Lock()
+		// m.messages[msg.Id] = msg
+		// m.ListenersLock.Unlock()
 
 		if msg.Expiration != 0 {
 			go func(msg *broker.Message) {
@@ -61,7 +71,8 @@ func (m *Module) Publish(ctx context.Context, subject string, msg broker.Message
 
 				<-ticker.C
 				m.ListenersLock.Lock()
-				delete(m.messages, msg.Id)
+				// delete(m.messages, msg.Id)
+				m.db.DeleteMessage(msg.Id, subject)
 				m.ListenersLock.Unlock()
 			}(&msg)
 		}
@@ -101,11 +112,15 @@ func (m *Module) Fetch(ctx context.Context, subject string, id int) (broker.Mess
 	case <-ctx.Done():
 		return broker.Message{}, broker.ErrInvalidID
 	default:
-		msg, ok := m.messages[id]
-		if ok {
-			return msg, nil
+		// msg, ok := m.messages[id]
+		// if ok {
+		// 	return msg, nil
+		// }
+		msg, err := m.db.FetchMessage(id)
+		if err != nil {
+			return broker.Message{}, broker.ErrExpiredID
 		}
 
-		return broker.Message{}, broker.ErrExpiredID
+		return msg, nil
 	}
 }
